3e54e05e123a8abea6583fdbc44fd62de2470fc3
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-01 02:55:09 -0800

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
     return (
-        <div>
+        <Fragment>
+            <Cart />
             <Header />
             <main>
                 <Meals />
             </main>
-        </div>
+        </Fragment>
     );
 }
 
