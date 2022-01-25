926085fb4a8a25964ed0856379a75e7eddf3a91d
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-24 23:20:17 -0800

    access input form values

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+    const formInputRef = useRef();
+
+    const submitFormHandler = (event) => {
+        event.preventDefault();
+
+        const enteredAmount = formInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (
+            enteredAmount.trim().length === 0 ||
+            enteredAmountNumber < 1 ||
+            enteredAmountNumber > 5
+        ) {
+            return;
+        }
+    };
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitFormHandler}>
             <Input
+                ref={formInputRef}
                 label="Amount"
                 input={{
                     id: "amount",
