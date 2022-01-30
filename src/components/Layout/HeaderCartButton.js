1ac006a9270a04584cb33eff5dede1742c44954a
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-30 00:00:25 -0800

    cart animation added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
-    const btnClasses = `${classes.button} ${classes.bump}`;
+    const btnClasses = `${classes.button} ${
+        btnIsHighlighted ? classes.bump : ""
+    }`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(true);
+        }, 300);
+
+        setBtnIsHighlighted(false);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
 
     return (
         <button className={btnClasses} onClick={props.onClick}>
